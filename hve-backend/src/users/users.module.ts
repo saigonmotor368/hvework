185c2e6e04187e80dee8aaad5e5541b1a91a7c77
HVE Developer <dev@hve.edu.vn>
2026-09-18 14:18:50 +0700

    fix: wire auth into user profiles module

--- hve-backend/src/users/users.module.ts
+++ hve-backend/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
